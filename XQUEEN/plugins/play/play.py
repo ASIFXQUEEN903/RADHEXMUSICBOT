b927bbf1ea5779cb167769481e1ee1ab65cd4005
RAJ RAUTTRAY <jasminbibiworkweb45@gmail.com>
2025-07-15 05:11:33 +0530

    XQUEEN

--- XQUEEN/plugins/play/play.py
+++ XQUEEN/plugins/play/play.py
@@ -615,54 +615,62 @@ async def slider_queries(client, CallbackQuery, _):
         cplay,
         fplay,
     ) = callback_request.split("|")
+
     if CallbackQuery.from_user.id != int(user_id):
         try:
             return await CallbackQuery.answer(_["playcb_1"], show_alert=True)
         except:
             return
+
     what = str(what)
     rtype = int(rtype)
+
     if what == "F":
         if rtype == 9:
             query_type = 0
         else:
             query_type = int(rtype + 1)
+
         try:
             await CallbackQuery.answer(_["playcb_2"])
         except:
             pass
+
         title, duration_min, thumbnail, vidid = await YouTube.slider(query, query_type)
-buttons = slider_markup(_, vidid, user_id, query, query_type, cplay, fplay)
-med = InputMediaPhoto(
-    media=thumbnail,
-    caption=_["play_10"].format(
-        title.title(),
-        duration_min,
-    ),
-    has_spoiler=True,
-)
-return await CallbackQuery.edit_message_media(
-    media=med, reply_markup=InlineKeyboardMarkup(buttons)
-)
+        buttons = slider_markup(_, vidid, user_id, query, query_type, cplay, fplay)
+        med = InputMediaPhoto(
+            media=thumbnail,
+            caption=_["play_10"].format(
+                title.title(),
+                duration_min,
+            ),
+            has_spoiler=True,
+        )
+        return await CallbackQuery.edit_message_media(
+            media=med, reply_markup=InlineKeyboardMarkup(buttons)
+        )
+
     if what == "B":
         if rtype == 0:
             query_type = 9
         else:
             query_type = int(rtype - 1)
+
         try:
             await CallbackQuery.answer(_["playcb_2"])
         except:
             pass
+
         title, duration_min, thumbnail, vidid = await YouTube.slider(query, query_type)
         buttons = slider_markup(_, vidid, user_id, query, query_type, cplay, fplay)
         med = InputMediaPhoto(
-    media=thumbnail,
-    caption=_["play_10"].format(
-        title.title(),
-        duration_min,
-    ),
-    has_spoiler=True,
- )
+            media=thumbnail,
+            caption=_["play_10"].format(
+                title.title(),
+                duration_min,
+            ),
+            has_spoiler=True,
+        )
         return await CallbackQuery.edit_message_media(
             media=med, reply_markup=InlineKeyboardMarkup(buttons)
         )
