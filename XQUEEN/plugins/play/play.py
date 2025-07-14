937f3c633c9f44a29931780cd3f6e1bd97e35281
RAJ RAUTTRAY <jasminbibiworkweb45@gmail.com>
2025-07-15 05:03:24 +0530

    XQUEEN

--- XQUEEN/plugins/play/play.py
+++ XQUEEN/plugins/play/play.py
@@ -632,18 +632,18 @@ async def slider_queries(client, CallbackQuery, _):
         except:
             pass
         title, duration_min, thumbnail, vidid = await YouTube.slider(query, query_type)
-        buttons = slider_markup(_, vidid, user_id, query, query_type, cplay, fplay)          
-         med = InputMediaPhoto(
-           media=thumbnail,
-       caption=_["play_10"].format(
-            title.title(),
-             duration_min,
-       ),
-         has_spoiler=True,
-       )
-        return await CallbackQuery.edit_message_media(
-            media=med, reply_markup=InlineKeyboardMarkup(buttons)
-        )
+buttons = slider_markup(_, vidid, user_id, query, query_type, cplay, fplay)
+med = InputMediaPhoto(
+    media=thumbnail,
+    caption=_["play_10"].format(
+        title.title(),
+        duration_min,
+    ),
+    has_spoiler=True,
+)
+return await CallbackQuery.edit_message_media(
+    media=med, reply_markup=InlineKeyboardMarkup(buttons)
+)
     if what == "B":
         if rtype == 0:
             query_type = 9
