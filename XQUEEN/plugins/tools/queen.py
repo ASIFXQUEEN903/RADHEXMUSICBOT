c32ea92cbba878f72ae0f83624b9cae79b852530
RAJ RAUTTRAY <jasminbibiworkweb45@gmail.com>
2025-07-15 12:01:43 +0530

    REPO

--- XQUEEN/plugins/tools/queen.py
+++ XQUEEN/plugins/tools/queen.py
@@ -7,19 +7,18 @@ from XQUEEN import app
 @app.on_message(filters.command("repo"))
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo="https://files.catbox.moe/f8i9s1.jpg",
+        photo="https://files.catbox.moe/ieqq3j.jpg",
         caption="""
 𓆩🍁𓆪 𝐇𝐞𝐲 𝐁𝐚𝐛𝐲 💞
 
-𓍯 𝐂𝐋𝐈𝐂𝐊 𝐎𝐍 𝐓𝐇𝐄 𝐁𝐔𝐓𝐓𝐎𝐍 𝐁𝐄𝐋𝐎𝐖 𝐓𝐎 𝐆𝐄𝐓 𝐓𝐇𝐈𝐒 𝐁𝐎𝐓'𝐒 𝐒𝐎𝐔𝐑𝐂𝐄 💫
-
-𓆩🩷 𝐌𝐀𝐃𝐄 𝐖𝐈𝐓𝐇 𝐋𝐎𝐕𝐄 𝐁𝐘 𝐗𝐐𝐔𝐄𝐄𝐍 𓆪
+ 𓆩 𝐐𝐔𝐄𝐄𝐍 𝐊𝐀 𝐎𝐅𝐅𝐈𝐂𝐀𝐋 𝐑𝐄𝐏𝐎 𓆪
+𓆩 399 ᴘᴀʏᴍᴇɴᴛ ᴋᴀʀᴋᴇ @ASHLF903 ᴀꜱᴋ ꜰᴏʀ ʀᴇᴘᴏ 𓆪
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "𓆩⚡ 𝙊𝙋𝙀𝙉 𝙍𝙀𝙋𝙊 ⚡𓆪", url="https://github.com/ASIFXQUEEN903/XQUEEN-MUSIC--903"
+                        "𓆩⚡ 𝘼𝙎𝙆 𝙁𝙊𝙍 ⚡𓆪", url="https://t.me/ASHLF903"
                     )
                 ]
             ]
