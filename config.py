c94c28a5da02abcb702988dc1b4993a61b7bfea2
RAJ RAUTTRAY <jasminbibiworkweb45@gmail.com>
2025-07-15 12:29:52 +0530

    XQUEEN

--- config.py
+++ config.py
@@ -78,12 +78,12 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/bem47n.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/izcz2e.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/9cevdg.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/izcz2e.jpg"
 )
-PLAYLIST_IMG_URL = "https://files.catbox.moe/i493lf.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/izcz2e.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/f8i9s1.jpg"
 TELEGRAM_AUDIO_URL = "https://files.catbox.moe/f8i9s1.jpg"
 TELEGRAM_VIDEO_URL = "https://files.catbox.moe/f8i9s1.jpg"
