e5836e9761e7837e2fad1c9da950ea1248610f0e
RAJ RAUTTRAY <jasminbibiworkweb45@gmail.com>
2025-07-15 13:57:29 +0530

    XQUEEN

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/jrupn9.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/5z3tfz.mp4"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/jrupn9.jpg"
